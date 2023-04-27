2c4b7817b2a81d6a6d601bd2490c6b23674eb54a
diddnjswns <diddnjswns20@gmail.com>
2023-04-27 14:02:33 +0900

    [design] 백테스팅 폼 변경

--- front/src/components/Pages/BackTesting.js
+++ front/src/components/Pages/BackTesting.js
@@ -1,26 +1,25 @@
-import React, { useState } from "react";
 import axios from "axios";
-import "../../css/Backtesting.css";
-import Parameter from "../modal/Parameter.js";
+
+import React, {useState} from "react";
+import '../../css/Backtesting.css'
+
 
 const BackTesting = () => {
   const [coinName, setCoinName] = useState("");
-  const [parameter, setParameter] = useState("");
+  const [rsi, setRsi] = useState("");
+  const [ma, setMa] = useState("");
+  const [ema, setEma] = useState("");
   const [term, setTerm] = useState("");
   const [testSize, setTestSize] = useState("");
   const [result, setResult] = useState("");
   const [showModal, setShowModal] = useState(false);
 
 
-  const handleModalClose = () => {
-    setShowModal(false);
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     const formData = {
       coin_name: coinName,
-      parameter,
+      parameter: { rsi, ma, ema },
       term,
       test_size: testSize,
     };
@@ -31,10 +30,19 @@ const BackTesting = () => {
       .catch((err) => console.log(err));
   };
 
+  const handleParameterClick = () => {
+    setShowModal(true);
+  };
+
+  const handleModalSave = () => {
+    setShowModal(false);
+    // Send data to server using axios.post
+  };
+
   return (
-    <div className="form-container">
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
+    <>
+      <form onSubmit={handleSubmit} className="backtesting-form">
+        <div>
           <label htmlFor="coinName">Coin Name:</label>
           <input
             type="text"
@@ -43,21 +51,7 @@ const BackTesting = () => {
             onChange={(e) => setCoinName(e.target.value)}
           />
         </div>
-
-        <div className="form-group">
-          <button onClick={() => setShowModal(true)}>
-            parameter
-          </button>
-          {
-            showModal == true ? <Parameter
-            showModal={showModal}
-            onClose={handleModalClose}
-            setParameter={setParameter}
-            /> : null
-          }
-        </div>
-
-        <div className="form-group">
+        <div>
           <label htmlFor="term">Term:</label>
           <input
             type="number"
@@ -66,8 +60,7 @@ const BackTesting = () => {
             onChange={(e) => setTerm(e.target.value)}
           />
         </div>
-
-        <div className="form-group">
+        <div>
           <label htmlFor="testSize">Test Size:</label>
           <input
             type="number"
@@ -76,17 +69,53 @@ const BackTesting = () => {
             onChange={(e) => setTestSize(e.target.value)}
           />
         </div>
-
-        <button type="submit" className="btn btn-primary">
-          Start Bot
-        </button>
         <div>
+          <button type="button" onClick={handleParameterClick}>
+            ADD Parameter
+          </button>
+        </div>
+        <button type="submit">Start Bot</button>
+        <div className="result-container">
+          <label htmlFor="result">Result</label>
           <pre>{result}</pre>
         </div>
       </form>
-    </div>
-  );
-
+      {showModal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h2>Add Parameter</h2>
+            <label htmlFor="rsi">RSI:</label>
+            <input
+              type="text"
+              id="rsi"
+              value={rsi}
+              onChange={(e) => setRsi(e.target.value)}
+            />
+            <label htmlFor="ma">MA:</label>
+            <input
+              type="text"
+              id="ma"
+              value={ma}
+              onChange={(e) => setMa(e.target.value)}
+            />
+            <label htmlFor="ema">EMA:</label>
+            <input
+              type="text"
+              id="ema"
+              value={ema}
+              onChange={(e) => setEma(e.target.value)}
+            />
+            <button type="button" onClick={handleModalSave}>
+              Save
+            </button>
+            <button type="button" onClick={() => setShowModal(false)}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  )
 };
 
 export default BackTesting;
