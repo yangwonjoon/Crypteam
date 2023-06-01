507bd7373bd574a8a9814ad9ff9b0d307800628e
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-06-01 18:56:18 +0900

    [MODIFY]백테스팅 차트 시각화

--- front/src/components/Pages/BackTesting.js
+++ front/src/components/Pages/BackTesting.js
@@ -1,10 +1,13 @@
 import axios from "axios";
-
-import React, {useState} from "react";
-import '../../css/Backtesting.css'
-
+import React, { useState, useEffect, useRef } from "react";
+import { createChart, CrosshairMode } from "lightweight-charts";
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+import "../../css/Backtesting.css";
 
 const BackTesting = () => {
+  const chartContainerRef = useRef(null);
+  const chartInstanceRef = useRef(null);
   const [coinName, setCoinName] = useState("");
   const [rsi, setRsi] = useState("");
   const [ma, setMa] = useState("");
@@ -14,6 +17,27 @@ const BackTesting = () => {
   const [result, setResult] = useState("");
   const [showModal, setShowModal] = useState(false);
 
+  useEffect(() => {
+    // 차트 생성
+    const chart = createChart(chartContainerRef.current, {
+      width: 800,
+      height: 400,
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+    });
+    chartInstanceRef.current = chart;
+
+    // 선 시리즈 생성
+    const lineSeries = chart.addLineSeries();
+    lineSeries.setData(
+      result && result.length > 0 ? result.map((data) => ({ time: data.time, value: data.close })) : []
+    );
+
+    return () => {
+      chart.remove();
+    };
+  }, [result]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -26,8 +50,27 @@ const BackTesting = () => {
     const url = "http://127.0.0.1:8000/api/start_bot/";
     axios
       .post(url, formData)
-      .then((res) => setResult(JSON.stringify(res.data)))
+      .then((res) => {
+        if (Array.isArray(res.data)) {
+          setResult(res.data);
+        } else {
+          setResult([]);
+        }
+      })
       .catch((err) => console.log(err));
+
+    // 상태 초기화
+    setCoinName("");
+    setRsi("");
+    setMa("");
+    setEma("");
+    setTerm("");
+    setTestSize("");
+    setResult([]); // 초기화: 빈 배열로 설정
+  };
+
+  const handleCoinChange = (e) => {
+    setCoinName(e.target.value);
   };
 
   const handleParameterClick = () => {
@@ -39,21 +82,41 @@ const BackTesting = () => {
     // Send data to server using axios.post
   };
 
+  const ChartContainer = styled("div")`
+    /* CSS 스타일링을 여기에 적용하세요 */
+  `;
+
+  const Modal = styled("div")`
+    /* CSS 스타일링을 여기에 적용하세요 */
+  `;
+
   return (
     <>
       <form onSubmit={handleSubmit} className="backtesting-form">
         <div>
           <label htmlFor="coinName">Coin Name:</label>
-          <input
-            type="text"
-            id="coinName"
-            value={coinName}
-            onChange={(e) => setCoinName(e.target.value)}
-          />
+          <FormControl>
+            <Select
+              id="coinName"
+              value={coinName}
+              onChange={handleCoinChange}
+              displayEmpty
+              inputProps={{ "aria-label": "Select Coin" }}
+            >
+              <MenuItem value="">
+                <em>Choose a coin</em>
+              </MenuItem>
+              <MenuItem value="BTC">BTC</MenuItem>
+              <MenuItem value="ETH">ETH</MenuItem>
+              <MenuItem value="ETC">ETC</MenuItem>
+              <MenuItem value="XRP">XRP</MenuItem>
+              <MenuItem value="ADA">ADA</MenuItem>
+            </Select>
+          </FormControl>
         </div>
         <div>
           <label htmlFor="term">Term:</label>
-          <input
+          <TextField
             type="number"
             id="term"
             value={term}
@@ -62,7 +125,7 @@ const BackTesting = () => {
         </div>
         <div>
           <label htmlFor="testSize">Test Size:</label>
-          <input
+          <TextField
             type="number"
             id="testSize"
             value={testSize}
@@ -70,52 +133,56 @@ const BackTesting = () => {
           />
         </div>
         <div>
-          <button type="button" onClick={handleParameterClick}>
+          <Button type="button" onClick={handleParameterClick}>
             ADD Parameter
-          </button>
+          </Button>
         </div>
-        <button type="submit">Start Bot</button>
-        <div className="result-container">
-          <label htmlFor="result">Result</label>
-          <pre>{result}</pre>
-        </div>
+        <Button type="submit">Start Bot</Button>
+
+          <div>
+            <label htmlFor="result">Result</label>
+            <ChartContainer ref={chartContainerRef} className="chart-container" />
+            {/* <pre>{JSON.stringify(result)}</pre> */}
+          </div>
+       
       </form>
       {showModal && (
-        <div className="modal">
-          <div className="modal-content">
+        <Modal>
+          <div>
             <h2>Add Parameter</h2>
             <label htmlFor="rsi">RSI:</label>
-            <input
+            <TextField
               type="text"
               id="rsi"
               value={rsi}
               onChange={(e) => setRsi(e.target.value)}
             />
             <label htmlFor="ma">MA:</label>
-            <input
+            <TextField
               type="text"
               id="ma"
               value={ma}
               onChange={(e) => setMa(e.target.value)}
             />
             <label htmlFor="ema">EMA:</label>
-            <input
+            <TextField
               type="text"
               id="ema"
               value={ema}
               onChange={(e) => setEma(e.target.value)}
             />
-            <button type="button" onClick={handleModalSave}>
+            <Button type="button" onClick={handleModalSave}>
               Save
-            </button>
-            <button type="button" onClick={() => setShowModal(false)}>
+            </Button>
+            <Button type="button" onClick={() => setShowModal(false)}>
               Close
-            </button>
+            </Button>
           </div>
-        </div>
+        </Modal>
       )}
+      
     </>
-  )
+  );
 };
 
 export default BackTesting;
