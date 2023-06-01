d129b3a6f1271a25069c72b60cd3a8a3fe8b873f
Jiyeon Bong <bbbccc113@tukorea.ac.kr>
2023-06-01 19:37:38 +0900

    [MODIFY] 백테스팅 차트 시각화

--- front/src/components/Pages/BackTesting.js
+++ front/src/components/Pages/BackTesting.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import React, { useState, useEffect, useRef } from "react";
 import { createChart, CrosshairMode } from "lightweight-charts";
-import { Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import {Paper, Grid, Modal, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
 import { styled } from "@mui/system";
 import "../../css/Backtesting.css";
 
@@ -83,104 +83,122 @@ const BackTesting = () => {
   };
 
   const ChartContainer = styled("div")`
-    /* CSS 스타일링을 여기에 적용하세요 */
-  `;
+  /* CSS 스타일링을 여기에 적용하세요 */
+`;
 
-  const Modal = styled("div")`
-    /* CSS 스타일링을 여기에 적용하세요 */
-  `;
+const StyledModal = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ModalContent = styled("div")`
+  background-color: white;
+  padding: 16px;
+  border-radius: 4px;
+`;
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="backtesting-form">
-        <div>
-          <label htmlFor="coinName">Coin Name:</label>
-          <FormControl>
-            <Select
-              id="coinName"
-              value={coinName}
-              onChange={handleCoinChange}
-              displayEmpty
-              inputProps={{ "aria-label": "Select Coin" }}
-            >
-              <MenuItem value="">
-                <em>Choose a coin</em>
-              </MenuItem>
-              <MenuItem value="BTC">BTC</MenuItem>
-              <MenuItem value="ETH">ETH</MenuItem>
-              <MenuItem value="ETC">ETC</MenuItem>
-              <MenuItem value="XRP">XRP</MenuItem>
-              <MenuItem value="ADA">ADA</MenuItem>
-            </Select>
-          </FormControl>
-        </div>
-        <div>
-          <label htmlFor="term">Term:</label>
-          <TextField
-            type="number"
-            id="term"
-            value={term}
-            onChange={(e) => setTerm(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="testSize">Test Size:</label>
-          <TextField
-            type="number"
-            id="testSize"
-            value={testSize}
-            onChange={(e) => setTestSize(e.target.value)}
-          />
-        </div>
-        <div>
-          <Button type="button" onClick={handleParameterClick}>
-            ADD Parameter
-          </Button>
-        </div>
-        <Button type="submit">Start Bot</Button>
+      <Grid container spacing={2} sx={{ paddingTop: 10, display: 'flex', justifyContent: 'center' }}>
+        <Grid item xs={12} sm={10}>
+          <Paper sx={{ padding: 2 }}>
+            <Grid item>
+              <Typography variant="h5">백테스팅</Typography>
+            </Grid>
+            <form onSubmit={handleSubmit} style={{ display: "flex", flexDirection: "row", gap: "16px" }}>
+              <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+                <label htmlFor="coinName">코인 이름:</label>
+                <FormControl>
+                  <Select
+                    id="coinName"
+                    value={coinName}
+                    onChange={handleCoinChange}
+                    displayEmpty
+                    inputProps={{ "aria-label": "Select Coin" }}
+                  >
+                    <MenuItem value="">
+                      <em>Choose a coin</em>
+                    </MenuItem>
+                    <MenuItem value="BTC">BTC</MenuItem>
+                    <MenuItem value="ETH">ETH</MenuItem>
+                    <MenuItem value="ETC">ETC</MenuItem>
+                    <MenuItem value="XRP">XRP</MenuItem>
+                    <MenuItem value="ADA">ADA</MenuItem>
+                  </Select>
+                </FormControl>
+              </div>
+              <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+                <label htmlFor="term">기간:</label>
+                <TextField
+                  type="number"
+                  id="term"
+                  value={term}
+                  onChange={(e) => setTerm(e.target.value)}
+                />
+              </div>
+              <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+                <label htmlFor="testSize">시작 날짜:</label>
+                <TextField
+                  type="number"
+                  id="testSize"
+                  value={testSize}
+                  onChange={(e) => setTestSize(e.target.value)}
+                />
+              </div>
+              <div style={{ display: "flex", alignItems: "flex-end", gap: "8px" }}>
+                <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
+                  <Button type="button" variant="contained" color="success" onClick={handleParameterClick}>
+                    ADD Parameter
+                  </Button>
+                  <Button type="submit" variant="contained" color="success">
+                    Start Bot
+                  </Button>
+                </div>
+              </div>
+            </form>
 
-          <div>
-            <label htmlFor="result">Result</label>
-            <ChartContainer ref={chartContainerRef} className="chart-container" />
-            {/* <pre>{JSON.stringify(result)}</pre> */}
-          </div>
-       
-      </form>
-      {showModal && (
-        <Modal>
-          <div>
-            <h2>Add Parameter</h2>
-            <label htmlFor="rsi">RSI:</label>
-            <TextField
-              type="text"
-              id="rsi"
-              value={rsi}
-              onChange={(e) => setRsi(e.target.value)}
-            />
-            <label htmlFor="ma">MA:</label>
-            <TextField
-              type="text"
-              id="ma"
-              value={ma}
-              onChange={(e) => setMa(e.target.value)}
-            />
-            <label htmlFor="ema">EMA:</label>
-            <TextField
-              type="text"
-              id="ema"
-              value={ema}
-              onChange={(e) => setEma(e.target.value)}
-            />
-            <Button type="button" onClick={handleModalSave}>
-              Save
-            </Button>
-            <Button type="button" onClick={() => setShowModal(false)}>
-              Close
-            </Button>
-          </div>
-        </Modal>
-      )}
-      
+            <div>
+              <label htmlFor="result">Result</label>
+              <ChartContainer ref={chartContainerRef} className="chart-container" />
+              {/* <pre>{JSON.stringify(result)}</pre> */}
+            </div>
+
+            <StyledModal open={showModal} onClose={() => setShowModal(false)}>
+              <ModalContent>
+                <h2>Add Parameter</h2>
+                <label htmlFor="rsi">RSI:</label>
+                <TextField
+                  type="text"
+                  id="rsi"
+                  value={rsi}
+                  onChange={(e) => setRsi(e.target.value)}
+                />
+                <label htmlFor="ma">MA:</label>
+                <TextField
+                  type="text"
+                  id="ma"
+                  value={ma}
+                  onChange={(e) => setMa(e.target.value)}
+                />
+                <label htmlFor="ema">EMA:</label>
+                <TextField
+                  type="text"
+                  id="ema"
+                  value={ema}
+                  onChange={(e) => setEma(e.target.value)}
+                />
+                <Button type="button" variant="contained" color="success" onClick={handleModalSave}>
+                  Save
+                </Button>
+                <Button type="button" variant="contained" color="success" onClick={() => setShowModal(false)}>
+                  Close
+                </Button>
+              </ModalContent>
+            </StyledModal>
+          </Paper>
+        </Grid>
+      </Grid>
     </>
   );
 };
