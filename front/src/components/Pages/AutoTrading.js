19d9704739d68cae81ea4df398bcfabec78f806d
diddnjswns <diddnjswns20@gmail.com>
2023-06-07 00:10:25 +0900

    [feat] 5초마다 get요청

--- front/src/components/Pages/AutoTrading.js
+++ front/src/components/Pages/AutoTrading.js
@@ -4,72 +4,96 @@ import '../../css/AutoTrading.css'
 import { Form, Button } from 'react-bootstrap';
 
 function AutoTrading() {
-  const [apiKey, setApiKey] = useState('');
-  const [secret, setSecret] = useState('');
-  const [symbol, setSymbol] = useState('');
-  const [leverage, setLeverage] = useState('');
-  const [data, setData] = useState(null);
-  const [intervalId, setIntervalId] = useState(null);
+    const [apiKey, setApiKey] = useState('');
+    const [secret, setSecret] = useState('');
+    const [symbol, setSymbol] = useState('');
+    const [leverage, setLeverage] = useState('');
+    const [data, setData] = useState(null);
+    const [intervalId, setIntervalId] = useState(null);
 
-  const handleSubmit = async (e) => {
+    const handleSubmit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://127.0.0.1:8000/api/AutoTradingView/', {
+        const response = await axios.post('http://127.0.0.1:8000/api/AutoTradingView/', {
         api_key: apiKey,
         secret: secret,
         symbol: symbol,
         leverage: leverage
-      });
+        });
 
-      setData(response.data);
+        setData(response.data);
 
-      // 시작 버튼을 눌렀을 때 5초마다 자동으로 GET 요청을 보내기 위해 setInterval을 설정합니다.
-      const id = setInterval(async () => {
-        const result = await axios.get('http://127.0.0.1:8000/api/AutoTradingResult/');
-        setData(result.data);
-      }, 5000);
-      setIntervalId(id);
+        // 시작 버튼을 눌렀을 때 5초마다 자동으로 GET 요청을 보내기 위해 setInterval을 설정합니다.
+        const id = setInterval(async () => {
+            const result = await axios.get('http://127.0.0.1:8000/api/AutoTradingResult/');
+            setData(result.data);
+        }, 5000);
+        setIntervalId(id);
     } catch (error) {
-      console.error(error);
+        console.error(error);
     }
-  };
+    };
 
-  const handleStop = () => {
+    const handleStop = () => {
     // 중단 버튼을 눌렀을 때 setInterval을 중지합니다.
     clearInterval(intervalId);
-  };
-
-  useEffect(() => {
-    return () => {
-      // 컴포넌트가 언마운트될 때 setInterval을 정리합니다.
-      clearInterval(intervalId);
     };
-  }, [intervalId]);
 
-  return (
+    useEffect(() => {
+        return () => {
+        // 컴포넌트가 언마운트될 때 setInterval을 정리합니다.
+        clearInterval(intervalId);
+    };
+    }, [intervalId]);
+
+    return (
     <>
-      <Form onSubmit={handleSubmit} className="autoform">
-        {/* 폼 컨트롤들... */}
+        <Form onSubmit={handleSubmit} className="autoform">
+        
+        <Form.Group controlId="apiKey">
+            <Form.Label>API Key: </Form.Label>
+            <Form.Control type="text" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="secret">
+            <Form.Label>Secret: </Form.Label>
+            <Form.Control type="text" value={secret} onChange={(e) => setSecret(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="symbol">
+            <Form.Label>Symbol: </Form.Label>
+            <Form.Control type="text" value={symbol} onChange={(e) => setSymbol(e.target.value)} />
+        </Form.Group>
+
+        <Form.Group controlId="leverage">
+            <Form.Label>Leverage: </Form.Label>
+            <Form.Control type="text" value={leverage} onChange={(e) => setLeverage(e.target.value)} />
+        </Form.Group>
+
         <Button variant="primary" type="submit">
-          시작
+            시작
         </Button>
-      </Form>
+        </Form>
+        <Button variant="danger" className="autostop" onClick={handleStop}>
+            중단
+        </Button>
 
-      {data ? (
-        <div className='autoresult'>
-          {/* 결과 값들... */}
-        </div>
-      ) : (
-        <p className='autoresult'>로딩중,,</p>
-      )}
-
-      {/* 중단 버튼 */}
-      <Button variant="danger" onClick={handleStop}>
-        중단
-      </Button>
+        {data ? (
+            <div className='autoresult'>
+                <p>현재 time: <span>{data.time}</span></p>
+                <p>현재 가격: <span>{data.price}</span></p>
+                <p>현재 수량: <span>{data.amount}</span></p>
+                <p>현재 평단가: <span>{data.average_price}</span></p>
+                <p>현재 수익률: <span>{data.ROE}%</span></p>
+                <p>현재 추세 예측(): <span>{data.pred}</span></p>
+                <p>수익: <span>{data.yeild}</span></p>
+            </div>
+        ) : (
+            <p className='autoresult'>로딩중,,</p>
+        )}
     </>
-  );
+    );
 }
 
 export default AutoTrading;
